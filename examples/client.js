06d053ab7838bd4e5afae5c42529cefc28706d2f
Laurent Zuijdwijk <you@example.com>
2016-01-14 21:13:26 +0000

    code styling

--- examples/client.js
+++ examples/client.js
@@ -5,17 +5,17 @@ var request = new Request();
 console.time('first');
 
 request.get('http://localhost:3000', {}, function (err, response) {
-    console.timeEnd('first');
-    console.log('first request, statusCode', response.statusCode);
-    console.time('second');
-    request.get('http://localhost:3000', {}, function (err, response) {
-        console.timeEnd('second');
-        console.log('second request, statusCode', response.statusCode);
-    });
-    console.time('third');
+  console.timeEnd('first');
+  console.log('first request, statusCode', response.statusCode);
+  console.time('second');
+  request.get('http://localhost:3000', {}, function (err, response) {
+    console.timeEnd('second');
+    console.log('second request, statusCode', response.statusCode);
+  });
+  console.time('third');
 
-    request.get('http://localhost:3000/aa', {}, function (err, response) {
-        console.timeEnd('third');
-        console.log('second request, statusCode', response.statusCode);
-    });
+  request.get('http://localhost:3000/aa', {}, function (err, response) {
+    console.timeEnd('third');
+    console.log('second request, statusCode', response.statusCode);
+  });
 });
--- examples/server.js
+++ examples/server.js
@@ -2,20 +2,20 @@
 
 var http = require('http');
 var etagValue = '686897696a7c876b7e';
-var server = new http.Server();
+var server = new http.Server()
 server.on('request', function (request, response) {
-    var etagRequest = request.headers['if-none-match'];
-    if (etagRequest === etagValue) {
-        response.writeHead(304);
-    }
-    else {
-        response.writeHead(200, {
-            'Content-Type': 'text/plain',
-            'ETag': etagValue
-        });
-        response.write(new Buffer(10000000));
-    }
-    response.end();
+  var etagRequest = request.headers['if-none-match'];
+  if (etagRequest === etagValue) {
+    response.writeHead(304);
+  }
+  else {
+    response.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'ETag': etagValue
+    });
+    response.write(new Buffer(10000000));
+  }
+  response.end();
 });
 
 server.listen(3000);
